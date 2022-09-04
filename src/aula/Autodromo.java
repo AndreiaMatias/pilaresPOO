ead1e4f4d70167152dfa26ceb395d274d16f3581
AndreiaMatias <deiamat@gmail.com>
2022-09-04 11:17:05 -0300

    Aula POO

---
+++ src/aula/Autodromo.java
@@ -0,0 +1,18 @@
+package aula;
+
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro jeep = new Carro();
+        jeep.setChassi("898977");
+        jeep.ligar();
+
+        Moto z400 = new Moto();
+        z400.setChassi("467754");
+        z400.ligar();
+
+        Veiculo coringa = jeep;
+        coringa.ligar();
+    }
+
+
+}
---
+++ src/aula/Moto.java
@@ -0,0 +1,9 @@
+package aula;
+
+public class Moto extends Veiculo{
+
+    @Override
+    public void ligar() {
+        System.out.println("MOTO LIGADA");
+    }
+}
